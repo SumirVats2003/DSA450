f3cfbfe6fb390e5a417cc7374ab2d5d70c38849c
Sumir Vats <sumirvats@gmail.com>
2022-12-24 22:38:13 +0530

    Added folders

---
+++ 1.Arrays/6_doUnion.cpp
@@ -0,0 +1,16 @@
+// Given two arrays a[] and b[] of size n and m respectively. The task is to find union between these two arrays.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+	// Function to return the count of number of elements in union of two arrays.
+	int doUnion(int a[], int n, int b[], int m)
+	{
+		// Brute force
+		// check if the element is present in larger array
+		int m[max(n, m)];
+	}
+};
